4f57b60afcc0b9c3620f473a20e056ad3a4a8cdd
Williamskj <89874192+Williamskj@users.noreply.github.com>
2021-10-02 17:14:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ // Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = "";
+for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+}
+document.getElementById("password").value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
